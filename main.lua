c91376ed987a418920dabe45d2558a21112d791a
sereen-ba <64044506+sereen-ba@users.noreply.github.com>
2020-06-08 17:24:18 +0300

    Add files via upload

--- main.lua
+++ main.lua
@@ -1,12 +1,25 @@
  function love.load()
-color ={155,99,222}
+color ={0,99,222}
 
 end
+function  love.update(dt)
+  function love.keypressed(key, scancode, isrepeat)
+
+ color ={math.random(255),math.random(255),math.random(255)}
+ love.event.poll()do
+   color ={math.random(255),math.random(255),math.random(255)}
+end
+end
+end
+
 function  love.draw()
 love.graphics.setBackgroundColor(color)
 end
 
-function love.keypressed()
-  color ={math.random(255),math.random(255),math.random(255)}
+
+function love.keypressed(key, scancode, isrepeat)
+   if key == "escape" then
+      love.event.quit()
+   end
 
 end
